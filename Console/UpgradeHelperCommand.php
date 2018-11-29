dd499f0eee13e1c1179db05e5da3f5cd77763363
Max Chadwick <mpchadwick@gmail.com>
2018-11-29 13:32:44 -0500

    Add a quick and dirty template override check

--- Console/UpgradeHelperCommand.php
+++ Console/UpgradeHelperCommand.php
@@ -40,6 +40,10 @@ class UpgradeHelperCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $report = [];
+        $report['PREFERENCES'] = [];
+        $report['TEMPLATES'] = [];
+
         // Build an array of preferences...
         $preferences = $this->objectManagerConfig->getPreferences();
         $preferenceArray = [];
@@ -58,12 +62,44 @@ class UpgradeHelperCommand extends Command
             if (strpos($line, 'diff -r') !== 0) {
                 continue;
             }
+            // Is there are preferences?
             $start = strpos($line, 'vendor');
             $end = strpos($line, ' ', $start);
             $file = substr($line, $start, $end - $start);
             if (array_key_exists($file, $preferenceArray)) {
-                $output->writeln('There is a preference for: ' . $file);
-                $output->writeln('--- preference: ' . $preferenceArray[$file]);
+                $report['PREFERENCES'][$file] = $preferenceArray[$file];
+            }
+
+            // Is there a template override?
+            // todo: Use the framework to detect template override
+            if (strpos($file, '.phtml') === false) {
+                continue;
+            }
+
+            $parts = explode('/', $file);
+            $last = end($parts);
+            $start = strpos($file, '/view/');
+            $end = strpos($file, $last);
+            $path = substr($file, $start, $end - $start);
+            $cmd = "find . -name " . $last . " -path '*" . $path . "*'";
+            $results = explode(PHP_EOL, trim(shell_exec($cmd)));
+            foreach ($results as $result) {
+                $result = substr($result, 2);
+                if (
+                    strpos($result, 'vendor/magento') === false &&
+                    $result != $file &&
+                    strpos($result, 'setup/view/magento') !== 0
+            ) {
+                    $report['TEMPLATES'][$file] = $result;
+                }
+            }
+        }
+
+        foreach ($report as $type => $items) {
+            $output->writeln('-------- ' . $type . ' --------');
+            foreach ($items as $patched => $customized) {
+                $output->writeln('Patched: ' . $patched);
+                $output->writeln('Customized: ' . $customized);
             }
         }
     }
